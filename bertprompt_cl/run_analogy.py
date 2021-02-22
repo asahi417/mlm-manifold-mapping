2f05e9c813cecafa70f417bc2577e7008b2324ad
asahi417 <asahi1992ushio@gmail.com>
2021-02-22 22:57:43 +0000

    init

--- bertprompt_cl/run_analogy.py
+++ bertprompt_cl/run_analogy.py
@@ -19,6 +19,23 @@ def get_options():
     return parser.parse_args()
 
 
+def get_best_prompt(file_list):
+
+    def safe_load(_file):
+        with open(_file, 'r') as f:
+            return json.load(f)
+
+    list_prompt = list(map(safe_load, file_list))
+    optimal_prompt = {}
+    for k in list_prompt[0].keys():
+        prompts = list(chain(*[p[k][0] for p in list_prompt]))
+        scores = list(chain(*[p[k][1] for p in list_prompt]))
+        assert len(prompts) == len(scores), '{} != {}'.format(len(prompts), len(scores))
+        best_index = scores.index(min(scores))
+        optimal_prompt[k] = [[prompts[best_index]], [scores[best_index]]]
+    return optimal_prompt
+
+
 def main():
     opt = get_options()
     level = logging.DEBUG if opt.debug else logging.INFO
@@ -27,8 +44,9 @@ def main():
     accuracy_full = {}
     list_prompt = glob('{}/prompt_dict*json'.format(opt.output_dir))
     # if opt.best:
-    #
-    # opt.reverse
+    #     prompts = [get_best_prompt(list_prompt)]
+    # else:
+    #     prompts
 
     for _file in list_prompt:
         logging.info('Running inference on {}'.format(_file))
@@ -39,6 +57,8 @@ def main():
         with open(_file, 'r') as f:
             prompt_dict = json.load(f)
         output_file = '{}/result.{}.{}.{}.{}.{}.pkl'.format(opt.output_dir, data, model, n_blank, n_blank_b, n_blank_e)
+        if opt.reverse:
+            output_file = output_file.replace('.pkl', '.reverse.pkl')
 
         if os.path.exists(output_file):
             with open(output_file, "rb") as fp:
@@ -46,17 +66,28 @@ def main():
             list_answer = [data['answer'] for data in full_data]
         else:
             prompter = bertprompt.Prompter(model, opt.length)
-            list_answer, list_prompt = [], []
+            list_answer, list_prompt, list_prompt_reverse = [], [], []
             for data in full_data:
                 list_answer.append(data['answer'])
                 h, t = data['stem']
                 all_template, all_score = prompt_dict['||'.join([h, t])]
+                all_template_r, all_score_r = prompt_dict['||'.join([t, h])]
                 template = all_template[-1]
+                template_r = all_template_r[-1]
                 assert h in template and t in template, '{} and {} not in {}'.format(h, t, template)
+                assert h in template_r and t in template_r, '{} and {} not in {}'.format(h, t, template_r)
                 list_prompt.append([template.replace(h, h_c).replace(t, t_c) for h_c, t_c in data['choice']])
+                list_prompt_reverse.append([template_r.replace(h, h_c).replace(t, t_c) for h_c, t_c in data['choice']])
+
             partition = bertprompt.get_partition(list_prompt)
             score = prompter.get_perplexity(list(chain(*list_prompt)), batch_size=opt.batch)
             score = [score[s:e] for s, e in partition]
+            if opt.reverse:
+                partition_r = bertprompt.get_partition(list_prompt_reverse)
+                score_r = prompter.get_perplexity(list(chain(*list_prompt_reverse)), batch_size=opt.batch)
+                score_r = [score_r[s:e] for s, e in partition_r]
+                score = list(map(lambda x: sum(x), zip(score, score_r)))
+
             with open(output_file, 'wb') as fp:
                 pickle.dump(score, fp)
         accuracy = []
